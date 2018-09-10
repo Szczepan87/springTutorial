04a1674bb31664eac8efe3a8988c1fccac737c64
Łukasz Szczepański <l.szczepan@wp.pl>
2018-09-10 14:24:07 +0200

    pierwszy commit, wersja bez bazy danych, swagger dla czytelności i testowania API

---
+++ src/main/java/com/szczepanski/App.java
@@ -0,0 +1,16 @@
+package com.szczepanski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Hello world!
+ */
+@EnableSwagger2
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/szczepanski/topic/TopicRepository.java
@@ -0,0 +1,9 @@
+package com.szczepanski.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic,String> {
+
+
+
+}
